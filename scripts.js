183957bb3acc1b01c8d49aa6772680f9366e4493
Dev Sebastian <devsebastian@gmail.com>
2019-11-09 17:27:59 +0530

    better UI, added 2 more sorts - heap and quick

--- scripts.js
+++ scripts.js
@@ -2,14 +2,87 @@ var arr = [];
 var play = false;
 
 var delay = 40;
-var iter = [0]
+var iter = [0, 0, 0]
 var multiplier = 1;
 
 updateDelay(document.getElementById("delay-slider").value);
 updateSize(document.getElementById("number-slider").value);
 updateSizeValue(document.getElementById("number-slider").value);
 
-var stack = []
+var l, r;
+async function sort() {
+    togglePlay();
+    if (play) {
+        delay = document.getElementById("delay-slider").value * multiplier;
+        var selection = document.getElementsByTagName("select")[0].selectedIndex
+        switch (parseInt(selection)) {
+            case 0:
+                selectionSort(iter);
+                break;
+            case 1:
+                BubbleSort(iter);
+                break;
+            case 2:
+                iter[0] += 1;
+                insertionSort(iter);
+                break;
+            case 3:
+                disableSort();
+                mergeSort(0, arr.length - 1);
+                break;
+            case 4:
+                disableSort();
+                await quickSort(0, arr.length - 1);
+                printChartOnly();
+                finishSort();
+                break;
+
+            case 5:
+                disableSort();
+                heapSort(arr.length);
+                console.log(arr);
+                break;
+        }
+    }
+}
+
+function enableSort() {
+    document.getElementById("sort-btn").innerHTML = "DISABLED";
+    document.getElementById("sort-btn").toggleAttribute("disabled");
+}
+
+function disableSort() {
+    document.getElementById("sort-btn").innerHTML = "DISABLED";
+    document.getElementById("sort-btn").toggleAttribute("disabled");
+}
+
+
+async function partition(l, r) {
+    var p = l;
+    while (l < r) {
+        while (arr[l] < arr[p]) {
+            l++;
+        }
+        while (arr[r] > arr[p]) {
+            r--;
+        }
+        if (l < r)
+            swap(l, r);
+    }
+    swap(p, r);
+    return r;
+}
+
+
+async function quickSort(l, r) {
+    if (l < r) {
+        var p = await partition(l, r);
+        printRangeCurr(l, r, p);
+        await sleep(delay);
+        await quickSort(l, p - 1);
+        await quickSort(p + 1, r);
+    }
+}
 
 function updateDelay(val) {
     delay = val * multiplier;
@@ -18,7 +91,6 @@ function updateDelay(val) {
 
 function updateSize(val) {
     iter[0] = 0;
-    stack = [];
     arr = [];
     for (var i = 0; i < val; i++) {
         arr.push(i + 1);
@@ -32,7 +104,7 @@ function updateSizeValue(val) {
     document.getElementById('number-value').innerHTML = "Size: " + val;
 }
 
-function shuffleArray(){
+function shuffleArray() {
     arr = shuffle();
     printChartOnly();
 }
@@ -53,37 +125,12 @@ function togglePlay() {
     document.getElementById("sort-btn").innerHTML = play ? "Pause" : "Sort";
 }
 
-function sort() {
-    togglePlay();
-    if (play) {
-        delay = document.getElementById("delay-slider").value * multiplier;
-        var selection = document.getElementsByTagName("select")[0].selectedIndex
-        switch (parseInt(selection)) {
-            case 0:
-                selectionSort(iter);
-                break;
-            case 1:
-                BubbleSort(iter);
-                break;
-            case 2:
-                iter[0] += 1;
-                insertionSort(iter);
-                break;
-            case 3:
-                document.getElementById("sort-btn").innerHTML = "DISABLED";
-                document.getElementById("sort-btn").toggleAttribute("disabled");
-                mergeSort(0, arr.length - 1);
-                break;
-        }
-    } 
-}
 
 function finishSort() {
     console.log("finished");
     printChartOnly();
     iter[0] = 0;
-    document.getElementById("sort-btn").innerHTML = "SORT";
-    document.getElementById("sort-btn").removeAttribute("disabled");
+    enableSort();
     togglePlay();
 }
 
@@ -106,7 +153,7 @@ function selectionSort(iter) {
                 pos = j;
             }
         }
-        printChart(iter[0], pos);
+        printRangeCurr(iter[0], arr.length - 1, pos);
         swap(iter[0], pos);
         if (play) {
             iter[0] += 1;
@@ -114,6 +161,37 @@ function selectionSort(iter) {
         }
     }
 }
+
+async function heapify(n, i) {
+    var largest = i; // Initialize largest as root 
+    var l = 2 * i + 1; // left = 2*i + 1 
+    var r = 2 * i + 2; // right = 2*i + 2 
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i) {
+        swap(i, largest);
+        await heapify(n, largest);
+    }
+}
+
+async function heapSort(n) {
+    var i;
+    for (i = n / 2 - 1; i >= 0; i--)
+        await heapify(n, i);
+    printRangeCurr(0, i, n);
+    await sleep(delay / 2);
+    for (i = n - 1; i >= 0; i--) {
+        swap(0, i);
+        await heapify(i, 0, i);
+        printRangeCurr(0, i, i);
+        await sleep(delay / 2);
+    }
+    printChartOnly();
+    finishSort();
+}
+
 
 
 function BubbleSort(iter) {
@@ -128,7 +206,7 @@ function BubbleSort(iter) {
             pos = j;
         }
     }
-    printChart(iter[0], pos);
+    printRangeCurr(0, pos, iter[0]);
     if (play) {
         iter[0] += 1;
         setTimeout(() => BubbleSort(iter), delay);
@@ -147,7 +225,7 @@ function insertionSort(iter) {
         j = j - 1;
     }
     arr[j + 1] = key;
-    printChart(iter[0], j);
+    printRangeCurr(iter[0], arr.length - 1, j);
     if (play) {
         iter[0] += 1;
         setTimeout(() => insertionSort(iter), delay);
@@ -162,19 +240,51 @@ function printChartOnly() {
     }
 }
 
-function printChart(curr, swap) {
+// function printChart(curr, swap) {
+//     var i;
+//     for (i = 0; i < arr.length; i++) {
+//         document.getElementsByClassName("bar")[i].setAttribute("style", "height: " + (arr[i] / max * 100) + "%;");
+//         if (i == curr) {
+//             document.getElementsByClassName("bar")[i].classList.add("curr");
+//         } else if (i == swap) {
+//             document.getElementsByClassName("bar")[i].classList.add("swap");
+//         } else {
+//             document.getElementsByClassName("bar")[i].classList.remove("swap");
+//             document.getElementsByClassName("bar")[i].classList.remove("curr");
+//         }
+
+//     }
+// }
+
+function printRange(l, r) {
     var i;
     for (i = 0; i < arr.length; i++) {
         document.getElementsByClassName("bar")[i].setAttribute("style", "height: " + (arr[i] / max * 100) + "%;");
-        if (i == curr) {
-            document.getElementsByClassName("bar")[i].classList.toggle("curr");
-        } else if (i == swap) {
-            document.getElementsByClassName("bar")[i].classList.toggle("swap");
+        if (i >= l && i <= r) {
+            document.getElementsByClassName("bar")[i].classList.add("unsorted");
         } else {
-            document.getElementsByClassName("bar")[i].classList.remove("swap");
             document.getElementsByClassName("bar")[i].classList.remove("curr");
+            document.getElementsByClassName("bar")[i].classList.remove("unsorted");
         }
-        
+
+    }
+}
+
+function printRangeCurr(l, r, curr) {
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        document.getElementsByClassName("bar")[i].setAttribute("style", "height: " + (arr[i] / max * 100) + "%;");
+        if (i >= l && i <= r) {
+            document.getElementsByClassName("bar")[i].classList.add("unsorted");
+        } else {
+            document.getElementsByClassName("bar")[i].classList.remove("curr");
+            document.getElementsByClassName("bar")[i].classList.remove("unsorted");
+        }
+        if (i == curr) {
+            document.getElementsByClassName("bar")[i].classList.remove("unsorted");
+            document.getElementsByClassName("bar")[i].classList.add("curr");
+        }
+
     }
 }
 
@@ -203,8 +313,6 @@ async function merge(l, m, r) {
     while (k <= r - m - 1) {
         arr[i++] = R[k++];
     }
-    await sleep(delay / 4);
-    printChart(r, l);
     if (l == 0 && r == arr.length - 1) {
         finishSort();
     }
@@ -215,13 +323,15 @@ async function mergeSort(l, r) {
 
     if (l < r) {
         var m = parseInt((l + r) / 2);
-        printChart(r, l);
-        await mergeSort(l, m);
-        printChart(r, l);
-        await mergeSort(m + 1, r);
-        printChart(r, l);
-        await merge(l, m, r);
-        await sleep(delay / 4);
+        printRange(l, r);
+        await sleep(delay / 3);
+        await mergeSort(l, m); // merge sort
+        printRange(l, m);
+        await sleep(delay / 3);
+        await mergeSort(m + 1, r);  // merge sOrt
+        printRange(m + 1, r);
+        await sleep(delay / 3);
+        await merge(l, m, r);   // finally merge them
     }
 }
 
